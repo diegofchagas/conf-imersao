522bc2499468f3c55a458ef3e3415755af3992d0
Diego Chagas <diego.chagas@afya.com.br>
2023-11-14 08:50:28 -0300

    encerrando as inscricoes

--- js/script.js
+++ js/script.js
@@ -1,5 +1,10 @@
 
+// const rsvpButton = document.getElementById("inscrever");
+// rsvpButton.addEventListener("click", function() {
+//  window.open('https://forms.gle/3Jf66sB73vgPfykP8')
+// });
+
 const rsvpButton = document.getElementById("inscrever");
-rsvpButton.addEventListener("click", function() {
- window.open('https://forms.gle/3Jf66sB73vgPfykP8')
+rsvpButton.addEventListener("click",function(){
+  return  alert('Inscrições Encerradas');
 });
